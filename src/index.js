0cd85fd20ff446af616dcd97800ac2239c25ccfe
Alex Kaminsky <alexander.s.kaminsky@gmail.com>
2018-12-31 09:50:20 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as PIXI from 'pixi.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = new PIXI.Application(800, 600, {backgroundColor : 0x000000});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+document.body.appendChild(app.view);
